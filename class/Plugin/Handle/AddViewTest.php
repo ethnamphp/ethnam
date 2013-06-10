9b9d250d37a6165195ed348f62399b1682c5cf3e
DQNEOO <DQNEOO@gmail.com>
2013-06-10 16:32:39 +0000

    use absolute path

--- class/Plugin/Handle/AddViewTest.php
+++ class/Plugin/Handle/AddViewTest.php
@@ -8,7 +8,7 @@
  *  @version    $Id$
  */
 
-require_once 'Ethna/class/Plugin/Handle/AddView.php';
+require_once ETHNA_BASE . '/class/Plugin/Handle/AddView.php';
 
 // {{{ Ethna_Plugin_Handle_AddViewTest
 /**
