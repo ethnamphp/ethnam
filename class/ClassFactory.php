800b589473067750713e53e562406056233b6223
DQNEOO <DQNEOO@gmail.com>
2013-06-14 07:34:50 +0000

    keep backword compatibility

--- class/ClassFactory.php
+++ class/ClassFactory.php
@@ -335,7 +335,7 @@ class Ethna_ClassFactory
      *
      *  @access protected
      */
-    function _include($class_name)
+    public function _include($class_name)
     {
         $file = sprintf("%s.%s", $class_name, $this->controller->getExt('php'));
         if (file_exists_ex($file)) {
