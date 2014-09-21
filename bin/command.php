796976667fca66e4de4a8bb9dcd0b0d1c872bb89
DQNEO <dqneoo@gmail.com>
2014-09-22 00:51:10 +0900

    this is not required because the autoloader do it.

--- bin/command.php
+++ bin/command.php
@@ -12,7 +12,6 @@ $base = dirname(dirname(dirname(__FILE__)));
 ini_set('include_path', $base.PATH_SEPARATOR.ini_get('include_path'));
 
 require_once __DIR__ . '/../bootstrap.php';
-require_once ETHNA_BASE . '/src/Getopt.php';
 
 // PEAR_Config violates the rule of E_STRICT
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
