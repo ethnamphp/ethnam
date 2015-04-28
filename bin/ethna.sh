22372849a5bf27f5b0dad3a2d31eb11d3ef3b6a9
DQNEO <dqneoo@gmail.com>
2015-04-28 17:14:01 +0900

    vendor/bin/ethna.sh exec vendor/dqneo/ethnam/bin/command.php

--- bin/ethna.sh
+++ bin/ethna.sh
@@ -11,11 +11,12 @@ if [ "$1" = "--debug" ]; then
     shift
 fi
 
+# vendor/bin
 THIS_DIR=$(cd $(dirname $0); pwd)
-ETHNA_HOME=$(dirname $THIS_DIR)
+VENDOR_DIR=$(dirname $THIS_DIR)
+ETHNAM_DIR=${VENDOR_DIR}/dqneo/ethnam
 CUR_DIR="$PWD"
-PHP_COMMAND="php"
 
 cd $CUR_DIR
 
-$PHP_COMMAND -d html_errors=off -d error_reporting="E_ALL & ~E_DEPRECATED" -qC $ETHNA_HOME/bin/command.php $*
+php -d html_errors=off -d error_reporting="E_ALL & ~E_DEPRECATED" -qC $ETHNAM_DIR/bin/command.php $*
