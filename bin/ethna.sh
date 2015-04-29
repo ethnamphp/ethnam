004f0a08c781c13a164e006e38db3adf02a66ffd
DQNEO <dqneoo@gmail.com>
2015-04-29 16:14:32 +0900

    this is totally useless

--- bin/ethna.sh
+++ bin/ethna.sh
@@ -15,8 +15,5 @@ fi
 THIS_DIR=$(cd $(dirname $0); pwd)
 VENDOR_DIR=$(dirname $THIS_DIR)
 ETHNAM_DIR=${VENDOR_DIR}/dqneo/ethnam
-CUR_DIR="$PWD"
-
-cd $CUR_DIR
 
 php -d html_errors=off -d error_reporting="E_ALL & ~E_DEPRECATED" -qC $ETHNAM_DIR/bin/command.php $*
