efea580d5053fd12fbeaf01227d5492e11e3c2ce
DQNEO <dqneoo@gmail.com>
2015-05-01 00:13:07 +0900

    add comment to be more readable

--- bin/command.php
+++ bin/command.php
@@ -8,9 +8,11 @@
  *  @license    http://www.opensource.org/licenses/bsd-license.php The BSD License
  *  @package    Ethna
  */
+
+ // suppose this file is located at 'project/vendor/dqneo/ethnam/bin/command.php'
 $binDir = __DIR__;
 $ethnamDir = $binDir . '/..';
-$vendorDir = $ethnamDir . '/../..';
+$vendorDir = $ethnamDir . '/../..'; // 'project/vendor'
 
 require_once  $vendorDir . '/autoload.php';
 
