f817a1a8c73875cc6f406442a38457fb386618ba
DQNEO <dqneoo@gmail.com>
2015-05-01 12:52:04 +0900

    refactor

--- skel/app.action.default.php
+++ skel/app.action.default.php
@@ -22,30 +22,6 @@ class {$project_id}_Form_Index extends {$project_id}_ActionForm
      *  @var      array   form definition.
      */
     public $form = array(
-       /*
-        *  TODO: Write form definition which this action uses.
-        *  @see http://ethna.jp/ethna-document-dev_guide-form.html
-        *
-        *  Example(You can omit all elements except for "type" one) :
-        *
-        *  'sample' => array(
-        *      // Form definition
-        *      'type'        => VAR_TYPE_INT,    // Input type
-        *      'form_type'   => FORM_TYPE_TEXT,  // Form type
-        *      'name'        => 'Sample',        // Display name
-        *
-        *      //  Validator (executes Validator by written order.)
-        *      'required'    => true,            // Required Option(true/false)
-        *      'min'         => null,            // Minimum value
-        *      'max'         => null,            // Maximum value
-        *      'regexp'      => null,            // String by Regexp
-        *
-        *      //  Filter
-        *      'filter'      => 'sample',        // Optional Input filter to convert input
-        *      'custom'      => null,            // Optional method name which
-        *                                        // is defined in this(parent) class.
-        *  ),
-        */
     );
 
     /**
