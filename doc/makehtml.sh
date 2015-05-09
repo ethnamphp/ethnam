95b64aa4e25404fd47f0b310714cb048cb26d9b0
DQNEO <dqneoo@gmail.com>
2015-05-09 23:10:35 +0900

    generate html files to another repository

--- doc/makehtml.sh
+++ doc/makehtml.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 set -e
-rm html -rf
-mkdir html
+target_dir=~/src/github.com/DQNEO/ethnamdoc
+
+cd $target_dir
+rm *.html
+cd -
 
 for i  in *.md
 do
-    ./convert.php $i html
+    ./convert.php $i $target_dir
 done
+
+cp $target_dir/README.html $target_dir/index.html
