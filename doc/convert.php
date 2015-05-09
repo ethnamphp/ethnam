e57c6ac5e76beb6cfe5cad6defa3e8d29e9e1210
DQNEO <dqneoo@gmail.com>
2015-05-09 21:53:03 +0900

    save to html directory

--- doc/convert.php
+++ doc/convert.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 global $argv;
-
+$targetDir = 'html';
 $file = $argv[1];
 // use github markdown
 
@@ -17,4 +17,4 @@ $mdContent = file_get_contents($file);
 $html = $Parsedown->text($mdContent);
 
 $htmlFile = preg_replace('/\.md$/', '.html', $file);
-file_put_contents($htmlFile, $html);
+file_put_contents($targetDir . '/' . $htmlFile, $html);
