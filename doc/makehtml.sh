4df1700447c91ba522df0a85c305ce07b135307a
DQNEO <dqneoo@gmail.com>
2015-05-09 23:13:21 +0900

    hard link

--- doc/makehtml.sh
+++ doc/makehtml.sh
@@ -3,13 +3,11 @@
 set -e
 target_dir=~/src/github.com/DQNEO/ethnamdoc
 
-cd $target_dir
-rm *.html
-cd -
+( cd $target_dir && rm *.html )
 
 for i  in *.md
 do
     ./convert.php $i $target_dir
 done
 
-cp $target_dir/README.html $target_dir/index.html
+cd $target_dir
