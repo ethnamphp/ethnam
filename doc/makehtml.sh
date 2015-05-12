353a6de8679d4a06eb5645a583b3671c13df98ed
DQNEO <dqneoo@gmail.com>
2015-05-12 19:06:51 +0900

    いったん全mdファイルをhtml化するのできた。

--- doc/makehtml.sh
+++ doc/makehtml.sh
@@ -3,14 +3,15 @@
 set -e
 target_dir=~/src/github.com/DQNEO/ethnamdoc
 
-( cd $target_dir && rm *.html )
+( cd $target_dir && rm -f *.html )
 
 for i  in *.md
 do
-    ./convert.php $i $target_dir
+    ./convert.php $i > $target_dir/${i%*.md}.html
 done
 
 cd $target_dir
+
 ln README.html index.html
 git add .
 git commit -m "auto generated"
